dafc6a9935b85dc01bf559364ae43f08768d76ac
Yurii Ivanes <ivanecura82@gmail.com>
2026-02-19 13:19:06 +0200

    JavaScript

---
+++ script.js
@@ -0,0 +1,105 @@
+// const username = "Poly";
+// const message = "Welcome " + username + "!";
+// console.log(message);
+
+// const guestName = "Mango";
+// const roomNumber = 207;
+// const greeting =
+//   "Welcome " + guestName + ", your room number is " + roomNumber + "!";
+// console.log(greeting);
+
+// const questName = "Mango";
+// const roomNumber = 207;
+// const greeting =
+//   "Welcome " + questName + ", your room number is " + roomNumber + "!";
+
+// console.log(greeting);
+
+// const guestName = "Mango";
+// const roomNumber = 207;
+// const greeting = `Welcome ${guestName}, your room number is ${roomNumber}!`;
+// console.log(greeting);
+
+// const guestName = "Mango";
+// const roomNumber = 207;
+// const greeting = `Welcome ${guestName}, your room number is ${roomNumber}!`;
+// console.log(greeting);
+
+// const productName = "Droid";
+//  const pricePerItem = 3500;
+
+// const message =
+//   "You picked " +
+//   productName +
+//   " price per item is " +
+//   pricePerItem +
+//   " credits ";
+
+// console.log(message);
+// const productName = "Droid";
+// const pricePerItem = 3500;
+
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits`;
+// console.log(message);
+
+// const username = "Poly";
+// const message = `Username ${username} is ${username.length} characters long`;
+
+// const username = "poly";
+// const message = `Username ${username} is ${username.length} characters long`;
+// console.log(message);
+
+// const courseTopic = "JavaScript essentials";
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[courseTopicLength - 1];
+// console.log(courseTopic);
+
+// const age = 16;
+// const isAdult = age >= 18;
+
+// const correctPassword = "jqueryismyjam";
+// const userPassword = "mangodab3st";
+// const isValid = correctPassword === userPassword;
+// console.log(isValid);
+
+// const correctPassword = "jqueryismyjam";
+// const userPassword = "mangodab3st";
+// const isValid = correctPassword === userPassword;
+// console.log(isValid);
+
+// const value = "24.5px";
+// const numerical = parseFloat(value);
+// console.log(numerical);
+
+// const value = "24.5px";
+// const numerical = parseFloat(value);
+// console.log(numerical);
+
+// function sayHi(sayHi) {
+//   console.log("Hello, this is my first function!")
+// }
+
+// function add(a, b, c) {
+//   const result = a + b + c;
+//   console.log(`Addition result equals ${result}`);
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// function add(a, b, c) {
+//   return a + b + c;
+// }
+
+// function makeMessage(name, price) {
+//   return `You picked ${name}, price per item is ${price} credits`;
+// }
